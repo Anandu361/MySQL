c25f227a5ee76525c252b8a8ecdb4d4695791e26
Anandu <anandusujakumari389@gmail.com>
2025-09-19 15:23:04 +0530

    Second assignment

--- login.sql
+++ login.sql
@@ -1,9 +1,10 @@
+-- SQL script to create a database to store user info
 CREATE DATABASE user_management;
 CREATE TABLE users(name VARCHAR(20), mail_id VARCHAR(20), password VARCHAR(20));
 INSERT INTO users VALUES('Anandu', 'anandusujakumari389@gmail.com', 'Anandu@123');
 INSERT INTO users VALUES('Aromal', 'aromal321@gmail.com', 'Aromal@123');
 
-
+-- SQL script to create a table to store user complaints
 CREATE TABLE user_complaints(name VARCHAR(20), complaint VARCHAR(100));
 INSERT INTO user_complaints VALUES('Anandu', 'App is not working properly');
 INSERT INTO user_complaints VALUES('Aromal', 'Unable to login');
